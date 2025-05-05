84d51539bb39227837cdfc89f879b90273145763
JiunCoding <lajiun@hotmail.com>
2025-05-05 20:32:54 +0800

    testing

---
+++ 01_Easy/09_Animodoro_App/09_Animodoro_App.py
@@ -0,0 +1,12 @@
+import time
+
+#Adding libraries
+#Let's make it simple with one main window and two layer, three buttons
+
+#creating the root
+
+#creating the main window
+
+#Let's add the counter in the (0,0)
+
+#Let's add the buttons in the (1.0)
